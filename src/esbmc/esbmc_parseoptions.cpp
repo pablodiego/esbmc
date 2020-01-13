107b30eb3a30029fdad03a0de1b52e3534043ba4
Mikhail Gadelha <mikhail.ramalho@gmail.com>
2020-01-13 19:23:48 -0400

    Update options of the k-induction parallel
    Signed-off-by: Mikhail Gadelha <mikhail.ramalho@gmail.com>

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -746,6 +746,9 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
     opts.set_option("forward-condition", false);
     opts.set_option("inductive-step", false);
 
+    opts.set_option("no-unwinding-assertions", true);
+    opts.set_option("partial-loops", false);
+
     // Start communication to the parent process
     close(forward_pipe[0]);
     close(backward_pipe[1]);
@@ -854,6 +857,10 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
     opts.set_option("forward-condition", true);
     opts.set_option("inductive-step", false);
 
+    opts.set_option("no-unwinding-assertions", false);
+    opts.set_option("partial-loops", false);
+    opts.set_option("no-assertions", true);
+
     // Start communication to the parent process
     close(forward_pipe[0]);
     close(backward_pipe[1]);
@@ -924,6 +931,9 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
     opts.set_option("forward-condition", false);
     opts.set_option("inductive-step", true);
 
+    opts.set_option("no-unwinding-assertions", true);
+    opts.set_option("partial-loops", false);
+
     // Start communication to the parent process
     close(forward_pipe[0]);
     close(backward_pipe[1]);
@@ -936,6 +946,9 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
     // 2. It couldn't find a proof
     for(u_int k_step = 2; k_step <= max_k_step; k_step += k_step_inc)
     {
+      if(opts.get_bool_option("disable-inductive-step"))
+        return true;
+
       bmct bmc(goto_functions, opts, context, ui_message_handler);
       set_verbosity_msg(bmc);
 
